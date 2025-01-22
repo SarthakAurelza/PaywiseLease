f8a66254831f1b1d4dbd48b7caaeda0343484011
sumedhav0501 <smedhav@aurelza.com>
2025-01-22 17:02:11 +0530

    responsiveness enhanced.

--- src/components/Disclaimer.jsx
+++ src/components/Disclaimer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Disclaimer = () => {
   return (
-    <div className='px-16 py-4 bg-[#003E511A]'>
+    <div className='p-4 xs:p-4 sm:p-6 md:p-8 lg:px-16 py-4 bg-[#003E511A] text-[11px] xs:text-xs md:text-sm lg:text-md'>
       <p> <span className='font-bold text-primary' >Disclaimer :</span> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
     </div>
   )
--- src/components/Request.jsx
+++ src/components/Request.jsx
@@ -2,23 +2,23 @@ import React from 'react';
 
 const Request = () => {
   return (
-    <div className="w-full p-16">
+    <div className="w-full lg:p-16 md:p-8 sm:p-6 p-4">
       <div className="bg-primary rounded-tl-xl rounded-tr-xl w-full h-full flex items-center justify-between p-8 overflow-hidden">
         {/* Text Content */}
-        <div className="w-[60%] flex flex-col items-start gap-16 z-10">
+        <div className="md:w-[60%] flex flex-col items-start gap-8 md:gap-16 z-10">
           <div className="flex flex-col gap-8">
-            <h2 className="text-white text-4xl md:text-5xl font-bold">Lease your car today</h2>
-            <p className="text-lg md:text-xl text-white">
+            <h2 className="text-white text-3xl sm:text-4xl md:text-5xl font-bold">Lease your car today</h2>
+            <p className="sm:text-lg sm:text-xl text-white">
               Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut.
             </p>
           </div>
-          <button className="bg-secondary px-[30px] py-[12px] rounded-3xl text-xl font-semibold">
+          <button className="bg-secondary px-6 sm:px-[30px] py-2 sm:py-[12px] rounded-3xl sm:text-xl font-semibold">
             Request a quote
           </button>
         </div>
 
         {/* Image Section */}
-        <div className="flex items-end w-[60%] h-full">
+        <div className="md:flex items-end w-[60%] h-full hidden ">
           <img
             className="scale-[1.8] translate-y-[-15%] -translate-x-[10%]"
             src="/images/banner.png"
