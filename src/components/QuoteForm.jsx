ecaf2858fd0e9984a7300f47c78d3117a7b9cdbe
sumedhav0501 <smedhav@aurelza.com>
2025-06-24 11:18:54 +0530

    form changes

--- src/components/QuoteForm.jsx
+++ src/components/QuoteForm.jsx
@@ -5,14 +5,14 @@ const QuoteForm = ({ handleSubmit }) => (
     initial={{ opacity: 0 }}
     animate={{ opacity: 1 }}
     exit={{ opacity: 0 }}
-    className="flex flex-col h-auto justify-between items-start w-full"
+    className="flex flex-col h-full justify-between items-start w-full"
   >
-    <form onSubmit={handleSubmit} className="flex flex-col gap-6 w-full">
+    <form onSubmit={handleSubmit} className="flex flex-col gap-6 w-full h-full">
       <input type="text" name="first_name" className="border p-2 rounded-md w-full border-primary" placeholder="First Name*" required />
       <input type="text" name="last_name" className="border p-2 rounded-md w-full border-primary" placeholder="Last Name*" required />
       <input type="email" name="email" className="border p-2 rounded-md w-full border-primary" placeholder="Enter your email" required />
       <input type="tel" name="phone" className="border p-2 rounded-md w-full border-primary" placeholder="Enter your phone number" required />
-      <textarea name="comment" className="border p-2 rounded-md w-full h-24 border-primary" placeholder="Any additional requests"></textarea>
+      <textarea name="comment" className="border p-2 rounded-md w-full h-60 border-primary" placeholder="Any additional requests"></textarea>
       <button type="submit" className="bg-muted text-white p-3 rounded-md text-lg font-semibold mt-4">Submit Quote Request</button>
     </form>
     <p className="mt-6">By clicking "submit", you acknowledge our Privacy Policy which contains a description of how we use your personal information.</p>
